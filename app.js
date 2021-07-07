356275df893d278596baa71f2812ec8ec28150e3
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-06 20:58:36 -0400

    create school class

---
+++ app.js
@@ -0,0 +1,31 @@
+class School {
+    constructor(name, level, numberOfStudents) {
+      this._name = name;
+      this._level = level;
+      this._numberOfStudents = numberOfStudents;
+    }
+    get name() {
+      return this._name;
+    }
+    get level() {
+      return this._level;
+    }
+    get numberOfStudents() {
+      return this._numberOfStudents;
+    }
+    set numberOfStudents(value) {
+      if(value.isNaN) {
+        console.log('Invaild input: numberOfStudents must be set to a number');
+      }
+      else {
+        this._numberOfStudents = value;
+      }
+    }
+    quickFacts() {
+      console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`);
+    }
+    static pickSubstituteTeacher(substituteTeachers) {
+      const randInt = Math.floor(Math.random() * substituteTeachers.length);
+      return substituteTeachers[randInt];
+    }
+}
